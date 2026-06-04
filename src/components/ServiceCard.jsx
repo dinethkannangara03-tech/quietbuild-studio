139991c7b92e8ff4d0515a55c21fbb9eb79d42f9
dinethkannangara03-tech <dinethkannangara03@gmail.com>
2026-06-04 19:50:02 +0530

    Update ServiceCard.jsx

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,10 +1,11 @@
 import { Link } from 'react-router-dom'
 import { ArrowIcon } from './Icons'
+import ContentIcon from './ContentIcon'
 
 function ServiceCard({ service }) {
   return (
     <article className="service-card">
-      <div className="service-icon">{service.icon}</div>
+      <div className="service-icon"><ContentIcon name={service.icon} /></div>
       <h2>{service.title}</h2>
       <p>{service.description}</p>
       <div className="suitable"><strong>Suitable for</strong><span>{service.suitable}</span></div>
